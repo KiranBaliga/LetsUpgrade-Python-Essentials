01bc7e2b06d93c4d9305cb8709c100a03893831a
Kiran Baliga B <68471170+KiranBaliga@users.noreply.github.com>
2020-07-20 15:47:54 +0530

    Create Day1 Assessment.py

---
+++ Day1 Assessment.py
@@ -0,0 +1,27 @@
+1)What is Python? What are the ways to install python in your PC?
+Python is an interpreted, object-oriented, high-level programming language. Python is simple, easy to learn. It reduces the cost of program maintenance. In Python, we can reuse the code. 
+It was developed by Guido Van Rossum in 1991 but came into limelight in 2006 when Google backed it up. 
+Python is a general-purpose coding language—which means that, unlike HTML, CSS, and JavaScript, it can be used for other types of programming 
+and software development besides web development.
+Python can be used for things like: Back end (or server-side) web and mobile app development.
+
+Some  ways to Install python in your system:
+A.Anaconda Navigator
+	- Use www.anaconda.com
+B.Python IDLE
+	- Use https://www.python.org/downloads/
+C.Pycharm
+	- Use https://www.jetbrains.com/pycharm/download/
+
+2) What are code environments and explain it's Types?
+Code Environment is a platform in which you can write, compile and run the code.
+A code environment is a standalone and self-contained environment to run Python. 
+In each location where you can run Python, you can select which code environment to use.
+
+Types of Code Environments:
+i.  Text Editor
+	eg:Notepad, VS code
+ii. Full IDEs
+	eg:pyCharm, Spyder
+iii.Notebook Environment
+	eg:Jupyter
