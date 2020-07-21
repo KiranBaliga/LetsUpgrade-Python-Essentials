34994469e4e5aba36869953bcc0721f4959752d8
Kiran Baliga B <68471170+KiranBaliga@users.noreply.github.com>
2020-07-21 17:25:43 +0530

    Add files via upload

---
+++ Day 2 Assessment.py
@@ -0,0 +1,439 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("hello world")
+
+
+# In[6]:
+
+
+print ("hello world")
+
+
+# In[8]:
+
+
+print ("hello\nworld")
+
+
+# In[9]:
+
+
+print ("hello\tworld")
+
+
+# In[16]:
+
+
+print ("hello\a")
+
+
+# In[13]:
+
+
+name = "kishan"
+marks = 90.87
+age = 34
+print("the name is ",name,"the marks are", marks,"the age is", age)
+
+
+# In[14]:
+
+
+print("the name is %s the marks are %f the age is %d"%(name,marks, age))
+
+
+# In[15]:
+
+
+print("the name is %10s the marks are %10.2f the age is %10d"%(name,marks, age))
+
+
+# In[19]:
+
+
+print(f"the name is {name} the marks are {marks} the age is {age}")
+
+
+# In[20]:
+
+
+_10=6
+
+
+# In[21]:
+
+
+if1=10
+
+
+# In[22]:
+
+
+cisco_temp=40
+
+
+# In[23]:
+
+
+print("the value is",if1)
+
+
+# In[30]:
+
+
+x = y = z = 20
+
+
+# In[25]:
+
+
+x
+
+
+# In[31]:
+
+
+z
+
+
+# In[32]:
+
+
+x = 25
+
+
+# In[35]:
+
+
+id(z)
+
+
+# In[36]:
+
+
+y=30
+
+
+# In[37]:
+
+
+id(y)
+
+
+# In[45]:
+
+
+del y
+
+
+# In[46]:
+
+
+x
+
+
+# In[47]:
+
+
+x=y=50
+
+
+# In[48]:
+
+
+del x
+
+
+# In[49]:
+
+
+y
+
+
+# In[51]:
+
+
+del y
+
+
+# In[52]:
+
+
+5**5
+
+
+# In[53]:
+
+
+10+20
+
+
+# In[54]:
+
+
+20-10
+
+
+# In[55]:
+
+
+10/2
+
+
+# In[56]:
+
+
+10/3
+
+
+# In[57]:
+
+
+10%3
+
+
+# In[58]:
+
+
+10//3
+
+
+# In[59]:
+
+
+a=10
+b=30
+a==b
+
+
+# In[60]:
+
+
+c=10
+a==c
+
+
+# In[61]:
+
+
+c<a
+
+
+# In[62]:
+
+
+c<=a
+
+
+# In[63]:
+
+
+b>=a
+
+
+# In[64]:
+
+
+a!=b
+
+
+# In[65]:
+
+
+a=240
+b=1
+a | b
+
+
+# In[66]:
+
+
+bin(a)
+
+
+# In[67]:
+
+
+bin(b)
+
+
+# In[68]:
+
+
+a & b
+
+
+# In[69]:
+
+
+a<<b
+
+
+# In[70]:
+
+
+a>>b
+
+
+# In[72]:
+
+
+a=1
+b=20
+a>20 or 10>9
+
+
+# In[73]:
+
+
+10>9 and 20> m
+
+
+# In[74]:
+
+
+10<9 and 20>m
+
+
+# In[75]:
+
+
+not 10>9
+
+
+# In[76]:
+
+
+not 10<9
+
+
+# In[78]:
+
+
+str1 = "pythonprogramming"
+"a" in str1
+
+
+# In[79]:
+
+
+"b" in str1
+
+
+# In[80]:
+
+
+"x" not in str1
+
+
+# In[81]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[82]:
+
+
+a is b
+
+
+# In[83]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[84]:
+
+
+a is b
+
+
+# In[85]:
+
+
+a is not b
+
+
+# In[86]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[87]:
+
+
+x is y
+
+
+# In[88]:
+
+
+id(x)
+
+
+# In[89]:
+
+
+id(y)
+
+
+# a=257
+# b=257
+# a==b
+
+# In[91]:
+
+
+a=257
+b=257
+a==b
+
+
+# In[92]:
+
+
+a is b
+
+
+# In[94]:
+
+
+10+10/29*4
+
+
+# In[95]:
+
+
+2**-1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
